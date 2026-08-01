1133bb58d0ca972fb1d3f993ebf56154bdd5075d
bd65 <bd65@students.waikato.ac.nz>
2026-08-01 13:41:11 +1200

    update styling

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,10 @@ export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
